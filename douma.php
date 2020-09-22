fd8a054ed41ac3e91bba8469ca4e26a1c040ef5c
tonyanangel <63249554+tonyanangel@users.noreply.github.com>
2020-09-22 20:12:06 +0700

    Create douma.php

---
+++ douma.php
@@ -0,0 +1,1 @@
+[{"ID":"1","HoTen":"Nguy\u1ec5n V\u0103n A","NamSinh":"1999","DiaChi":"C\u00e0 Mau"},{"ID":"2","HoTen":"Tr\u1ea7n V\u0103n B","NamSinh":"2000","DiaChi":"C\u1ea7n Th\u01a1"},{"ID":"3","HoTen":"L\u00ea Th\u1ecb C","NamSinh":"1998","DiaChi":"Kh\u00e1nh H\u00f2a"},{"ID":"4","HoTen":"Nguy\u1ec5n V\u0103n D","NamSinh":"1996","DiaChi":"Ngh\u1ec7 An"}]
